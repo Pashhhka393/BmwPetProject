7a2d8fb4fb66b2bfa064a49484f43c450d2eb7ed
Pashhhka393 <pseveluhin@gmail.com>
2025-07-09 19:28:19 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const setups = [
-  { video: document.getElementById('header-video'), icon: document.getElementById('soundAudio'), btn: document.getElementById('button') },
   { video: document.getElementById('video-auto'), icon: document.getElementById('soundAudio'), btn: document.getElementById('button') },
+  { video: document.getElementById('video-auto-2'), icon: document.getElementById('soundAudio2'), btn: document.getElementById('button2') },
 ];
 
 setups.forEach(({video, icon, btn}) => {
